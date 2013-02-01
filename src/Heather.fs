5ab09ffbfd960b69127aa387f4b4514126659a9a
Heather <Heather@cynede.net>
2013-02-01 13:21:40 +0400

    wait for exit anyways

--- src/Heather.fs
+++ src/Heather.fs
@@ -12,6 +12,7 @@ module Shell =
         proc.UseShellExecute <- false
         proc.Arguments <- args
         System.Diagnostics.Process.Start(proc) |> ignore
+        p.WaitForExit()
     let shell cmd args =
         let proc = new System.Diagnostics.ProcessStartInfo(cmd)
         proc.RedirectStandardOutput <- true
